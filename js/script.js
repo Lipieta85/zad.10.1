0efc763dba47caece7864d71a6af9171c083ebe4
Rafał Lipieta <cwgflojdid@wp.pl>
2018-05-29 14:05:58 +0200

    Add jquery change hash scroll button

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  hash: true,
-  pageDots: false,
-  cellAlign: 'center',
-  contain: true
+$(document).ready(function () {
+
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem, {
+
+    hash: true,
+    pageDots: false,
+    cellAlign: 'center',
+    contain: true
+  });
+
+
+  var $carousel = $('.main-carousel').flickity();
+  var $progressBar = $('.progress-bar');
+
+  $carousel.on('scroll.flickity', function (event, progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    $progressBar.width(progress * 100 + '%');
+  });
+
+
+  var carouselData = Flickity.data($carousel[0])
+  var progress = (carouselData.selectedIndex + 1) / carouselData.cells.length
+  $progressBar.width(progress * 100 + '%');
+
+  $('.button-group').on('click', '.button', function () {
+    var index = $(this).index();
+    $carousel.flickity('selectCell', index);
+  });
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
- 
-  
-});
-
-var $carousel = $('.main-carousel').flickity();
-
-var $progressBar = $('.progress-bar');
-
-$carousel.on( 'scroll.flickity', function( event, progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  $progressBar.width( progress * 100 + '%' );
-});
-
-var $carousel = $('.main-carousel').flickity();
-
-$('.button-group').on( 'click', '.button', function() {
-  var selector = $(this).attr('data-selector');
-  $carousel.flickity( 'selectCell', selector );
-});

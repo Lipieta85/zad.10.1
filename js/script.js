3816c4eb61d5a3f80f915a76476a8c11aebd0be9
Rafał Lipieta <cwgflojdid@wp.pl>
2018-05-31 15:49:57 +0200

    Mustache Carousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
 
+  var templateList = document.getElementById('template-product-list').innerHTML;
+
+  Mustache.parse(templateList);
+
+  var list = '';
+
+  for (var i = 0; i < productsData.length; i++) {
+    console.log(productsData);
+    list += Mustache.render(templateList, productsData[i]);
+  }
+
+  results.insertAdjacentHTML('afterend', list);
+
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem, {
+
+    hash: true,
+    pageDots: false,
+    cellAlign: 'center',
+    contain: true
+  });
+
   var $carousel = $('.main-carousel').flickity();
   var $progressBar = $('.progress-bar');
 
@@ -17,4 +39,3 @@ $(document).ready(function () {
     $carousel.flickity('selectCell', index);
   });
 });
-
